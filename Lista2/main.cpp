da8ec142296f4d10b564e97e3e26f977495e97a6
ghost000 <radixblade100@gmail.com>
2018-03-04 12:10:41 +0100

    zad2 list 2

--- Lista2/main.cpp
+++ Lista2/main.cpp
@@ -9,25 +9,24 @@ std::mt19937_64 rng;
 auto zad1(int N = 100, int NN = 10000) {
 
     Gnuplot gp;
-    std::vector<std::vector<boost::tuple<int, int, int, int> > > pts(NN, std::vector<boost::tuple<int, int, int, int>>(N));
-    int polozenie {0};
+    std::vector<std::vector<boost::tuple<int, int, int, int> > > pts(NN,
+                                                                     std::vector<boost::tuple<int, int, int, int>>(N));
+    int polozenie{0};
     std::uniform_int_distribution<int> zero_one(-1, 0);
-    for(auto i {0}; i < pts.size(); i++)
-    {
-        for(auto j {0}; j < pts.at(i).size(); j++)
-        {
-            pts.at(i).at(j) = boost::make_tuple(j,polozenie,1,1);
-            polozenie += zero_one(rng)*2+1;
+    for (auto i{0}; i < pts.size(); i++) {
+        for (auto j{0}; j < pts.at(i).size(); j++) {
+            pts.at(i).at(j) = boost::make_tuple(j, polozenie, 1, 1);
+            polozenie += zero_one(rng) * 2 + 1;
         }
         polozenie = 0;
     }
-    std::string plot {"plot "};
-    for(auto i {0}; i < NN; i++)
+    std::string plot{"plot "};
+    for (auto i{0}; i < NN; i++)
         plot += " '-' with lines title '', ";
     plot += "\n";
     gp << "set xrange [-1:101]\nset yrange [-40:40]\n";
     gp << plot;
-    for(const auto& i : pts)
+    for (const auto &i : pts)
         gp.send1d(i);
 
 }
@@ -35,28 +34,44 @@ auto zad1(int N = 100, int NN = 10000) {
 auto zad2(double p = 1.3, int N = 100, int NN = 100) {
 
     Gnuplot gp;
-    std::vector<std::vector<boost::tuple<double, double, double, double> > > pts(NN, std::vector<boost::tuple<double, double, double, double>>(N));
-    double polozenie {0.00};
+    std::vector<std::vector<boost::tuple<double, double, double, double> > > pts(NN,
+                                                                                 std::vector<boost::tuple<double, double, double, double>>(
+                                                                                         N));
+    double polozenie{0.00};
+    double suma{0.00};
+    double srednia{0.00};
+    double odchylenie{0.00};
     std::uniform_int_distribution<int> zero_one(-1, 0);
-    for(auto i {0}; i < pts.size(); i++)
-    {
-        for(auto j {0}; j < pts.at(i).size(); j++)
-        {
-            pts.at(i).at(j) = boost::make_tuple(j,polozenie,1,1);
-            polozenie += (zero_one(rng)*2+1)*p;
+    for (auto i{0}; i < pts.size(); i++) {
+        for (auto j{0}; j < pts.at(i).size(); j++) {
+            pts.at(i).at(j) = boost::make_tuple(j, polozenie, 1, 1);
+            polozenie += (zero_one(rng) * 2 + 1) * p;
         }
+        suma += polozenie;
         polozenie = 0;
     }
-    std::string plot {"plot "};
-    for(auto i {0}; i < NN; i++)
+
+    srednia = suma / NN;
+    suma = 0;
+
+    for (auto i{0}; i < pts.size(); i++) {
+        for (auto j{0}; j < pts.at(i).size(); j++) {
+            suma = suma + pow((pts.at(i).at(j).get<1>() - srednia), 2);
+        }
+    }
+    odchylenie = sqrt(suma / NN);
+    std::cout << "odchylenie standardowe : " << odchylenie << std::endl;
+    std::string plot{"plot "};
+    for (auto i{0}; i < NN; i++)
         plot += " '-' with lines title '', ";
     plot += "\n";
     gp << "set xrange [-1:101]\nset yrange [-40:40]\n";
     gp << plot;
-    for(const auto& i : pts)
+    for (const auto &i : pts)
         gp.send1d(i);
 
 }
+
 int main() {
     rng.seed(static_cast<unsigned long>(time(nullptr)));
     //zad1();
