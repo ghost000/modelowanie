3cc4ff147a580b12851025b57f6d3465763b86ed
ghost000 <radixblade100@gmail.com>
2018-04-18 21:14:03 +0200

    y

--- Lista2/main.cpp
+++ Lista2/main.cpp
@@ -1,49 +1,54 @@
 #include <iostream>
 #include <random>
 #include <ctime>
-#include "../gnuplot-iostream/gnuplot-iostream.h"
+#include <fstream>
 
 std::mt19937_64 rng;
 
 //zad1
-auto zad1(int N = 100, int NN = 10000) {
+auto zad1(unsigned N = 100, int NN = 1000)
+{
+    std::ofstream myExcelFile;
+    myExcelFile.open("zad1a.csv");
 
-    Gnuplot gp;
-    std::vector<std::vector<std::tuple<int, int, int, int> > > pts(NN,
-                                                                     std::vector<std::tuple<int, int, int, int>>(N));
-    int polozenie{0};
+    auto polozenie{0.0};
     std::uniform_int_distribution<int> zero_one(-1, 0);
-    for (auto i{0}; i < pts.size(); i++) {
-        for (auto j{0}; j < pts.at(i).size(); j++) {
-            pts.at(i).at(j) = std::make_tuple(j, polozenie, 1, 1);
+
+    for (auto j{0}; j < NN; j++)
+    {
+        polozenie += zero_one(rng) * 2 + 1;
+        myExcelFile << 1 << "\t" << polozenie << "\t" << std::endl;
+    }
+    myExcelFile.close();
+
+    polozenie = 0; 
+
+    std::ofstream myExcelFile2;
+    myExcelFile2.open("zad1b.csv");
+
+    for (auto i{0}; i < N; i++)
+    {
+        for (auto j{0}; j < NN; j++)
+	{
             polozenie += zero_one(rng) * 2 + 1;
-        }
+	}
+        myExcelFile2 << i << "\t" << polozenie << std::endl;
         polozenie = 0;
     }
-    std::string plot{"plot "};
-    for (auto i{0}; i < NN; i++)
-        plot += " '-' with lines title '', ";
-    plot += "\n";
-    gp << "set xrange [-1:101]\nset yrange [-40:40]\n";
-    gp << plot;
-    for (const auto &i : pts)
-        gp.send1d(i);
-
+    myExcelFile2.close();
 }
+/*
+auto zad2(double p = 1.3, int N = 100, int NN = 1000) {
 
-auto zad2(double p = 1.3, int N = 100, int NN = 100) {
-
-    Gnuplot gp;
-    std::vector<std::vector<std::tuple<double, double, double, double> > > pts(NN,
-                                                                                 std::vector<std::tuple<double, double, double, double>>(
-            N));
+    std::vector<std::vector<std::tuple<double, double, double, double> > > pts(NN, std::vector<std::tuple<double, double, double, double>>(N));
     double polozenie{0.00};
     double suma{0.00};
     double srednia{0.00};
     double odchylenie{0.00};
     std::uniform_int_distribution<int> zero_one(-1, 0);
     for (auto i{0}; i < pts.size(); i++) {
-        for (auto j{0}; j < pts.at(i).size(); j++) {
+        for (auto j{0}; j < pts.at(i).size(); j++)
+        {
             pts.at(i).at(j) = std::make_tuple(j, polozenie, 1, 1);
             polozenie += (zero_one(rng) * 2 + 1) * p;
         }
@@ -61,14 +66,6 @@ auto zad2(double p = 1.3, int N = 100, int NN = 100) {
     }
     odchylenie = sqrt(suma / NN);
     std::cout << "odchylenie standardowe : " << odchylenie << std::endl;
-    std::string plot{"plot "};
-    for (auto i{0}; i < NN; i++)
-        plot += " '-' with lines title '', ";
-    plot += "\n";
-    gp << "set xrange [-1:101]\nset yrange [-40:40]\n";
-    gp << plot;
-    for (const auto &i : pts)
-        gp.send1d(i);
 
 }
 
@@ -119,11 +116,11 @@ auto zad3()
     gp << "plot '-' with lines title ''\n";
     gp.send1d(pts);
 }
-
+*/
 int main() {
     rng.seed(static_cast<unsigned long>(time(nullptr)));
-    //zad1();
-    zad2();
+    zad1();
+    //zad2();
     //zad2(0.7);
     //zad3();
 
