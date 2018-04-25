74b85435812c61c2f939fdb7c945597fe8513678
ghost000 <radixblade100@gmail.com>
2018-04-25 07:51:11 +0200

    t'

---
+++ Lista2/cmake-build-debug/CMakeFiles/Lista2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lista2.dir/main.cpp.o"
+  "Lista2.pdb"
+  "Lista2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lista2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
